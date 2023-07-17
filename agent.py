8bdd561bdca8048b6c6d7b1bb636c1f56abef57a
hc <huangchengb@163.com>
2023-07-17 12:59:13 +0800

    basically determined the structure

--- agent.py
+++ agent.py
@@ -8,21 +8,21 @@ class QNetwork(nn.Module):
     def __init__(self, state_dim: int, action_dim: int, action_scale: int):
         super(QNetwork, self).__init__()
         # shared state feature extraction layer
-        self.feature = nn.Sequential(nn.Linear(state_dim, 256),
+        self.feature = nn.Sequential(nn.Linear(state_dim, 512),
                                      nn.ReLU(),
-                                     nn.Linear(256, 128),
+                                     nn.Linear(512, 256),
                                      nn.ReLU()
                                      )
         # evaluate action advantages on each branch
-        self.actions = [nn.Sequential(nn.Linear(128, 64),
+        self.actions = [nn.Sequential(nn.Linear(256, 128),
                                       nn.ReLU(),
-                                      nn.Linear(64, action_scale)
+                                      nn.Linear(128, action_scale)
                                       ) for _ in range(action_dim)]
         self.actions = nn.ModuleList(self.actions)
         # module to calculate state value
-        self.value = nn.Sequential(nn.Linear(128, 64),
+        self.value = nn.Sequential(nn.Linear(256, 128),
                                    nn.ReLU(),
-                                   nn.Linear(64, 1)
+                                   nn.Linear(128, 1)
                                    )
 
     def forward(self, x):
@@ -30,14 +30,14 @@ class QNetwork(nn.Module):
         actions = torch.stack([head(feature) for head in self.actions])
         value = self.value(feature)
         # 1 mean max
-        # maxa = actions.mean(-1).max(0)[0].unsqueeze(-1)
-        # actions = actions - maxa + value
+        maxa = actions.mean(-1).max(0)[0].unsqueeze(-1)
+        actions = actions - maxa + value
 
         # 2 min max max
-        mina = actions.min(-1)[0].unsqueeze(-1)
-        actions = actions - mina
-        maxa = actions.max(0)[0].max(-1)[0].unsqueeze(-1)
-        actions = actions - maxa  + value
+        # mina = actions.min(-1)[0].unsqueeze(-1)
+        # actions = actions - mina
+        # maxa = actions.max(0)[0].max(-1)[0].unsqueeze(-1)
+        # actions = actions - maxa  + value
 
         # 3 min mean max
         # mina = actions.min(-1)[0].unsqueeze(-1)
@@ -74,12 +74,14 @@ class BDQ(nn.Module):
         q_values = self.q(state).transpose(0, 1)  # q_values for all possible actions
         q_values = q_values.gather(2, actions.long()).squeeze(-1)  # get q_values for current action
 
-        # select best actions from Q and calculate Q-values in target Q
+        # select best actions from Q and calculate Q-values in target Q; DDQN
         max_next_action = self.q(next_state).transpose(0, 1).max(-1, keepdim=True)[1]
-        next_q_values = self.target_q(next_state).transpose(0, 1)  # normal dqn
+        next_q_values = self.target_q(next_state).transpose(0, 1)
         max_next_q_values = next_q_values.gather(2, max_next_action.long()).squeeze(-1)  # get q_values for next action
-        # next_q_values = self.target_q(next_state).transpose(0, 1)
+        
+        # next_q_values = self.target_q(next_state).transpose(0, 1)  # normal dqn
         # max_next_q_values = next_q_values.max(-1, keepdim=True)[0].squeeze(-1)
+
         q_target = (done_mask * gamma * max_next_q_values + reward)
 
         loss = F.mse_loss(q_values, q_target)
